14855275e74cd8b16ef726d8d2efa7e039ddf723
Tania <taniapapazaf@gmail.com>
2016-02-01 18:35:50 +0000

    Added a toolbar button
    This button will eventually allow the user to add the current tab to the
    list of bookmarks

---
+++ popup.js
@@ -0,0 +1,7 @@
+// Called when the user clicks on the browser action.
+/*chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log('need to add this to chrome.storage');
+});*/
+
+// Not logging anything (why?)
+console.log('popup.js loaded');
