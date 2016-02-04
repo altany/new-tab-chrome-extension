f209396aeef8e65913af37b50c23e8cc759494ba
Tania <taniapapazaf@gmail.com>
2016-02-04 20:48:35 +0000

    Limited when scripts run
    Limited the scripts to the pages needed only.

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
-
-/*function init() {
-	console.log('popup.js loaded');   
-} */
+function init() {
+	
+	console.log('loaded');
+	document.getElementById('addSite').addEventListener('click', add); 
+}
 
 function add() {
     chrome.storage.sync.get('counter', function(data) {
@@ -12,11 +13,39 @@ function add() {
 		chrome.storage.sync.set({'counter' : counter}, function() {});
 		
 		console.log(counter, document.location.href);
+		/*var details = {
+			title : $('#addLinkModal #title').val(),
+			url : $('#addLinkModal #url').val()
+		};
+
+		var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+		if ($('#addLinkModal #title').val()=='') {
+			alert('Please enter a title for your bookmark');
+			return false;
+		}
+		else if (!re.test(details.url)) { 
+			alert('Please enter a valid URL');
+			return false;
+		}
+		counter++;		
+
+		addLink(counter, details.url, details.title);
+
+		details.position = $('#id-'+counter).position();
+		var bookmark = {};
+		bookmark[counter] = details;
+		console.log(bookmark);
+		storage.set(bookmark, function() {});
+
+		$( '#addLinkModal' ).hide();
+		$('#addLinkModal input').val('');
+
+		storage.set({'counter' : counter}, function() {});*/
 	}); 
 	
 	
 } 
 
 
-//document.addEventListener('DOMContentLoaded', init);
-document.getElementById('addSite').addEventListener('click', add);
+document.addEventListener('DOMContentLoaded', init);
+
